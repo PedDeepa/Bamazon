139ed8fa280d4a21b94676ecfe2c397536c1693a
Deepa Peddamail <dpm676@ads.northwestern.edu>
2018-06-14 17:17:24 -0500

    SQLFiles_Upload

---
+++ bamazonDBandTableDDLs.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE bamazonDB; 
+
+CREATE TABLE bamazonDB.products 
+  ( 
+     item_id            INTEGER(11) AUTO_INCREMENT NOT NULL, 
+     product_name       VARCHAR(256),
+     department_id      INTEGER(11),
+     department_names   VARCHAR(256), 
+     price              DECIMAL(10,2), 
+     stock_quantity     INTEGER(10),
+     PRIMARY KEY (item_id) 
+  ); 
+  
+ALTER TABLE bamazonDB.products AUTO_INCREMENT = 1001;
+
+  SELECT * FROM bamazonDB.products;
+
+  CREATE TABLE bamazonDB.departments 
+  ( 
+     department_id      INTEGER(11),
+     department_names   VARCHAR(256), 
+     over_head_costs    DECIMAL(10,2), 
+     product_sales      DECIMAL(10,2),
+     total_profit       DECIMAL(10,2),
+     PRIMARY KEY (department_id ) )
